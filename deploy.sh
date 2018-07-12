fc2a920198f3428d89d3014e8f5403c95f2a1d90
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-12 17:52:25 +0700

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm run build:prod
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
-git clone https://efrataretailindo@dl-ui-dev.scm.azurewebsites.net:443/dl-ui-dev.git out
+git clone https://$USERNAME:$PASSWORD@dl-ui-dev.scm.azurewebsites.net:443/dl-ui-dev.git out
 
 cp -a dist/. out/.
 cd out
