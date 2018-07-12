f1f17269c0ec58db9c7bf72c77999d0dbe4b2bb6
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-12 17:26:57 +0700

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,12 @@ npm run build:prod
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
-git clone https://${GH_TOKEN}@github.com/danliris/danliris.github.io.git out
+git clone https://efrataretailindo@dl-ui-dev.scm.azurewebsites.net:443/dl-ui-dev.git out
 
 cp -a dist/. out/.
 cd out
+
+git add .
+git commit -m "Automated Deployment"
+
+git push origin master
